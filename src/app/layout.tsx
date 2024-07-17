47834154e59e2d24fd692492434aa822d1e9b93a
Andras Horvath <horvathandrasmate@gmail.com>
2024-07-17 15:19:14 +0200

    add posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import './globals.css'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import ReactQueryProvider from '@/providers/ReactQueryProvider'
 import { ClerkProvider } from '@clerk/nextjs'
+import dynamic from 'next/dynamic'
+import { PHProvider } from './providers'
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -17,6 +19,10 @@ export const metadata = {
   description:
     'The fastest way to get your documents for the Physicist-Engineer course',
 }
+
+const PostHogPageView = dynamic(() => import('@/components/PostHogPageView'), {
+  ssr: false,
+})
 
 export default function RootLayout({
   children,
@@ -30,24 +36,27 @@ export default function RootLayout({
         className={GeistSans.className}
         style={{ colorScheme: 'dark' }}
       >
-        <body className="bg-background text-foreground">
-          <NextTopLoader showSpinner={false} height={2} color="#2acf80" />
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <ReactQueryProvider>
-              <main className="flex min-h-screen flex-col items-center">
-                {children}
-                <Analytics />{' '}
-                {/* ^^ remove this if you are not deploying to vercel. See more at https://vercel.com/docs/analytics  */}
-              </main>
-              <ReactQueryDevtools initialIsOpen={false} />
-            </ReactQueryProvider>
-          </ThemeProvider>
-        </body>
+        <PHProvider>
+          <body className="bg-background text-foreground">
+            <NextTopLoader showSpinner={false} height={2} color="#2acf80" />
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <ReactQueryProvider>
+                <main className="flex min-h-screen flex-col items-center">
+                  <PostHogPageView />
+                  {children}
+                  <Analytics />{' '}
+                  {/* ^^ remove this if you are not deploying to vercel. See more at https://vercel.com/docs/analytics  */}
+                </main>
+                <ReactQueryDevtools initialIsOpen={false} />
+              </ReactQueryProvider>
+            </ThemeProvider>
+          </body>
+        </PHProvider>
       </html>
     </ClerkProvider>
   )
